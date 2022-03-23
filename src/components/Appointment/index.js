d435cce009cd180521c6de1ee073761b135aa009
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-23 14:44:21 -0700

    add Appointment component, styles, fix bug with DayList onChange

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,8 @@
+import react from "react";
+import "components/Appointment/styles.scsss";
+
+export default function Appointment(props) {
+  return(
+    <article className="appointment"></article>
+  );
+}
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -3,15 +3,14 @@ import DayListItem from "./DayListItem";
 
 
 export default function DayList(props) {
-  const days = props.days;
-  const listDays = days.map((item => {
+  const listDays = props.days.map((item => {
     return (
         <DayListItem
           key={item.id} 
           name={item.name} 
           spots={item.spots}
           selected={item.name === props.value}
-          setDay={() => props.onChange(props.name)}
+          setDay={() => props.onChange(item.name)}
         />)
   })); 
   return (
