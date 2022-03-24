2398cd464f82deb229c68bd6abcbf904910c1899
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-24 16:25:29 -0700

    add get request for Days data with axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,26 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
 import "components/Application.scss";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 const appointments = {
   "1": {
@@ -62,7 +46,15 @@ const appointments = {
 };
 
 export default function Application(props) {
+  const [days, setDays] = useState([]);
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days").then(response => {
+    setDays([...response.data]);
+    });
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
