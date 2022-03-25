0238f4ec22ac748447c4035ced13825468494482
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-24 18:22:33 -0700

    group states to one object

--- src/components/Application.js
+++ src/components/Application.js
@@ -46,8 +46,13 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get("http://localhost:8001/api/days").then(response => {
@@ -65,7 +70,7 @@ export default function Application(props) {
       />
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
-        <DayList days={days} value={day} onChange={setDay}/>
+        <DayList days={state.days} value={state.day} onChange={setDay}/>
       </nav>
       <img
         className="sidebar__lhl sidebar--centered"
