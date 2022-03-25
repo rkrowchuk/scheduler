52c5254767d3a6069ebfb79f2e8c7a1c08251c3e
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-25 15:56:58 -0700

    create useVisualMode hook and tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+
+export function useVisualMode(initial) {
+const [mode, setMode] = useState(initial);
+const [history, setHistory] = useState([initial]);
+
+const transition = (newMode, replace = false) => {
+  setMode(newMode);
+  if (replace) {
+    setHistory(prev => [...prev.slice(0, prev.length-1), newMode]);
+  } else {
+    setHistory(prev => [...prev, newMode]);
+  }
+}
+const back = () => {
+  if (history.length < 2) {
+    return;
+  }
+  setMode(history[history.length-2]);
+  setHistory(prev => [...prev.slice(0, prev.length-1)]);
+}
+  return { mode, transition, back, history };
+} 
