20e2f1504027786e7e068fa054aac082e3533051
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-25 17:06:36 -0700

    implement CREATE mode into appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,11 +3,12 @@ import "components/Appointment/styles.scss";
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 import { useVisualMode } from "hooks/useVisualMode";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
-
+const CREATE = "CREATE";
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
@@ -16,12 +17,15 @@ export default function Appointment(props) {
   return(
     <article className="appointment">
       <Header time={props.time}/>
-      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === SHOW && (
       <Show
         student={props.interview.student}
         interviewer={props.interview.interviewer}
       />
+      )}
+      {mode === CREATE && (
+        <Form interviewers={[]}/>
       )}
     </article>
   );
