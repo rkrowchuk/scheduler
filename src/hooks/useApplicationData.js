573ccb28c66d0ad8861c70fc2357d6b42fcc17b9
rkrowchuk <rhiannonkrowchuk@gmail.com>
2022-03-30 18:35:46 -0700

    implement cypress tests for appointments

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -35,40 +35,13 @@ export function useApplicationData() {
     return axios.put(`/api/appointments/${id}`, {interview})
       .then((response) => {
         setState({...state, appointments});
-        updateSpots();
       })
   }
 
   const cancelInterview = (id) => {
    return axios.delete(`/api/appointments/${id}`)
       .then((response) => {
-        updateSpots();
       })
-  }
-
-  const updateSpots = function(state, appointments, id) {
-
-    const oldInterview = state.appointments[id].interview;
-    const newInterview = appointments[id].interview;
-    let update = 0;
-  
-    if (oldInterview !== null && newInterview !== null) {
-      update = 0;
-    }
-    if (oldInterview !== null && newInterview === null) {
-      update = 1;
-    }
-    if (oldInterview === null && newInterview !== null) {
-      update = -1;
-    }
-    const dayObj = state.days.find(d1 => d1.name === state.day);
-    const newDays = state.days.map(day => {
-      if (dayObj) {
-        return {...day, spots: day.spots + update}
-      }
-      return day;
-    })
-    return newDays;
   }
 
   return {
@@ -76,7 +49,6 @@ export function useApplicationData() {
     setDay,
     bookInterview,
     cancelInterview, 
-    updateSpots
   }
 
 }
